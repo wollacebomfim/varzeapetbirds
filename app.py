3efabb0ba5050364dd06b89119ff2f9be2b1de5b
root <root@srv592377.hstgr.cloud>
2025-07-23 09:36:45 -0300

    Configurando app.py

--- app.py
+++ app.py
@@ -24,17 +24,17 @@ app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY') or 'uma_chave_secreta'
 
 # Configuração do MySQL
 app.config['MYSQL_HOST'] = os.environ.get('MYSQL_HOST', '127.0.0.1')
-app.config['MYSQL_USER'] = os.environ.get('MYSQL_USER', 'appmeutreino')
-app.config['MYSQL_PASSWORD'] = os.environ.get('MYSQL_PASSWORD', 'Littleboy@1944')
+app.config['MYSQL_USER'] = os.environ.get('MYSQL_USER', '')
+app.config['MYSQL_PASSWORD'] = os.environ.get('MYSQL_PASSWORD', '')
 app.config['MYSQL_DB'] = os.environ.get('MYSQL_DB', 'loja_virtual')
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 # Configuração de e-mail
 app.config['MAIL_SERVER'] = 'smtp.hostinger.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'no-reply@appmeutreino.com'
-app.config['MAIL_PASSWORD'] = 'Enolagay@1945'
-app.config['MAIL_DEFAULT_SENDER'] = 'no-reply@appmeutreino.com'
+app.config['MAIL_USERNAME'] = ''
+app.config['MAIL_PASSWORD'] = ''
+app.config['MAIL_DEFAULT_SENDER'] = ''
 mail = Mail(app)
 
 # Configuração do logger
